37573ee422256c7edafd7abac054b2fdd0cdd33b
gmarino95 <steveray1995@gmail.com>
2016-11-10 16:53:50 +0100

    RomanNumerals Version 1.0

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,63 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
 		
+		int decimal = 0;
+		int lastNumber = 0;
+		
+		String romanNumeral = romanNum.toUpperCase();
+		
+		for(int i = romanNumeral.length() - 1; i >= 0; i--){
+			
+			char convertToDecimal = romanNum.charAt(i);
+			
+			switch (convertToDecimal){
+				case'M': 
+					decimal = processDecimal(1000, lastNumber, decimal);
+					lastNumber = 1000;
+					break;
+				
+				case 'D':
+					decimal = processDecimal(500, lastNumber, decimal);
+					lastNumber = 500;
+					break;
+					
+				case 'C':
+					decimal = processDecimal(100, lastNumber, decimal);
+					lastNumber = 100;
+					break;
+					
+				case 'L':
+					decimal = processDecimal(50, lastNumber, decimal);
+					lastNumber = 50;
+					break;
+					
+				case 'X':
+					decimal = processDecimal(10, lastNumber, decimal);
+					lastNumber = 10;
+					break;
+					
+				case 'V':
+					decimal = processDecimal(5, lastNumber, decimal);
+					lastNumber = 5;
+					break;
+					
+				case 'I':
+					decimal = processDecimal(1, lastNumber,decimal);
+					lastNumber = 1;
+					break;
+			}	
+		}
+		return decimal;	
+	}
+	
+	public int processDecimal(int decimal, int lastNumber, int lastDecimal){
+		
+		if(lastNumber > decimal){
+			return lastDecimal;
+		}
+		else{
+			return lastDecimal + decimal;
+		}
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,36 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
+	
+	private int decimal;
+	private String romanNum;
+	private RomanNumerals roman= new RomanNumerals();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testonecharacter() {
+		
+		romanNum = "V";
+		decimal = roman.convertToInteger(romanNum);
+		
+		assertEquals(5, decimal);
+	}
+	
+	@Test
+	public void testthreecharacter(){
+		
+		romanNum = "III";
+		decimal = roman.convertToInteger(romanNum);
+		
+		assertEquals(3, decimal);
+	}
+	
+	@Test
+	public void testNumber(){
+		
+		romanNum = "MMXV";
+		decimal = roman.convertToInteger(romanNum);
+		
+		assertEquals(2015, decimal);
 	}
 
 }
